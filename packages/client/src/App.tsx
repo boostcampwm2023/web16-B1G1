e1f8ecb9eca7826dba315fc8f1da731f68f00bb0
KimGaeun <greencloud11@hanyang.ac.kr>
2023-11-13 17:38:33 +0900

    ✨ Set page route
    - Landing 페이지, Home 페이지 생성
    - App.tsx에 router 세팅

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,15 @@
 import './App.css';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Landing from './pages/Landing';
 
 export default function App() {
-	return '';
+	return (
+		<HashRouter>
+			<Routes>
+				<Route index element={<Landing />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</HashRouter>
+	);
 }

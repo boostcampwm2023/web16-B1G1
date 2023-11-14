8809598f09fe2011bc5212b935a5af733e8f17ba
KimGaeun <greencloud11@hanyang.ac.kr>
2023-11-14 21:26:30 +0900

    ✨ Make star size same when the camera is moving away
    - 별과 카메라 사이 거리가 5000 이상이 되면, 별이 더 이상 작아지지 않고 일정한 크기를 유지하도록 처리함
    
    Co-authored-by: bananaba <78800560+bananaba@users.noreply.github.com>
    Co-authored-by: MinboyKim <kdm3801@gmail.com>

--- packages/client/src/components/feature/Star/index.tsx
+++ packages/client/src/components/feature/Star/index.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
+import { DIST_LIMIT } from 'constants/star';
 
 interface PropsType {
 	position: THREE.Vector3;
@@ -11,6 +12,17 @@ interface PropsType {
 export default function Star({ position, size, color }: PropsType) {
 	const meshRef = useRef<THREE.Mesh>(null!);
 
+	useFrame((state, delta) => {
+		const distance = meshRef.current.position.distanceTo(state.camera.position);
+		const scale = distance / DIST_LIMIT;
+
+		if (distance > DIST_LIMIT) {
+			meshRef.current.scale.x = scale;
+			meshRef.current.scale.y = scale;
+			meshRef.current.scale.z = scale;
+		}
+	});
+
 	return (
 		<mesh ref={meshRef} position={position}>
 			<sphereGeometry args={[size, 32, 16]} />
@@ -18,7 +30,6 @@ export default function Star({ position, size, color }: PropsType) {
 				color={color}
 				emissive={color}
 				emissiveIntensity={2}
-				toneMapped={false}
 			/>
 		</mesh>
 	);

185e6e8b08d9cebb2daf1ae07e75100713ed22d2
qkrwogk <pjha999@naver.com>
2023-11-14 13:48:35 +0900

    ✨ generated board resource
    - nest g resource 명령을 활용해 board 모듈, 컨트롤러, 서비스 및
      CRUD generator로 기본 CRUD 메소드까지 추가
    - 관련 패키지 설치
    
    Co-authored-by: 송준섭 <wnstjq7979@gmail.com>

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })

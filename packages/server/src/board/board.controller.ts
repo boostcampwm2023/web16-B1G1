e19a563d06ff15bb7ffaa3e2eace3b486c20b7ed
qkrwogk <pjha999@naver.com>
2023-11-15 17:02:18 +0900

    ✨ handle POST /board
    - e2e 테스트 코드가 통과되도록 Controller, Service 코드 구현
    - Respository 주입하여 MySQL 연동

--- packages/server/src/board/board.controller.ts
+++ packages/server/src/board/board.controller.ts
@@ -10,13 +10,14 @@ import {
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { Board } from './entities/board.entity';
 
 @Controller('board')
 export class BoardController {
 	constructor(private readonly boardService: BoardService) {}
 
 	@Post()
-	create(@Body() createBoardDto: CreateBoardDto) {
+	create(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
 		return this.boardService.create(createBoardDto);
 	}
 
--- packages/server/src/board/board.service.ts
+++ packages/server/src/board/board.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './entities/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BoardService {
-	create(createBoardDto: CreateBoardDto) {
-		return 'This action adds a new board';
+	constructor(
+		@InjectRepository(Board)
+		private boardRepository: Repository<Board>,
+	) {}
+
+	async create(createBoardDto: CreateBoardDto): Promise<Board> {
+		const { title, content, author } = createBoardDto;
+
+		const board = await this.boardRepository.create({
+			title,
+			content,
+			author,
+		});
+		const created: Board = await this.boardRepository.save(board);
+
+		return created;
 	}
 
 	findAll() {

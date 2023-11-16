7508301e63c260d62d94cfccc54e8295ce3ffe0b
qkrwogk <pjha999@naver.com>
2023-11-16 17:28:13 +0900

    ✅ added it.todo on auth module
    - 백로그 이슈 분석하여 auth module에서 개발되어야 할 API 계획 작성
    - OAuth2.0 회원가입/로그인의 경우 e2e테스트가 힘들기 때문에 TDD에서 제외함
    
    Co-authored-by: 송준섭 <wnstjq7979@gmail.com>

---
+++ packages/server/test/auth/auth.e2e-spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+
+describe('AuthController (/auth, e2e)', () => {
+	let app: INestApplication;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	// #12 [02-05] 서버는 아이디 중복을 검사하고 결과를 클라이언트에 전송한다.
+	it.todo('GET /auth/check-duplicate-username');
+
+	// #91 [02-12] 서버는 닉네임 중복을 검사하고 결과를 클라이언트에 전송한다.
+	it.todo('GET /auth/check-duplicate-nickname');
+
+	// #16 [02-09] 서버는 회원가입 데이터를 받아 형식 검사와 아이디 중복검사를 진행한다.
+	// #17 [02-10] 검사에 통과하면 회원 정보를 데이터베이스에 저장한다.
+	it.todo('POST /auth/signup');
+
+	// #20 [03-02] 사용자가 정보제공을 허용하여 콜백 API 요청을 받으면, 백엔드 서버는 요청에 포함된 코드를 통해 해당 서비스의 인가 서버에 액세스 토큰을 요청한다.
+	// #21 [03-03] 액세스 토큰을 전달받으면, 백엔드 서버는 액세스 토큰을 통해 해당 서비스의 리소스 서버에 사용자 정보를 요청한다.
+	// #22 [03-04] 사용자 정보를 전달받으면, 필요한 속성만 추출하여 회원 정보를 데이터베이스에 저장한다.
+	it.todo('GET /auth/oauth/:service'); // OAuth2.0 서비스 로그인 페이지로 리다이렉트, 쿼리 스트링으로 client_id, scope 담아서 보냄
+	it.todo('GET /auth/oauth/:service/callback');
+	// OAuth2.0 서비스 로그인 후 리다이렉트, 쿼리 스트링으로 code 담아서 보냄.
+	// 백엔드 서버는 해당 서비스의 인가 서버에 액세스 토큰을 요청한다.
+	// 액세스 토큰을 전달받으면, 백엔드 서버는 액세스 토큰을 통해 해당 서비스의 리소스 서버에 사용자 정보를 요청한다.
+	// 사용자 정보를 받으면, 유저 테이블을 조회하여 이미 가입한 회원인지 확인한다.
+	// 이미 가입한 회원이라면, JWT를 발급하고 쿠키에 저장한다. -> JWT 리턴하는지 확인
+	// 새로운 회원이라면, 클라이언트에 닉네임 정보를 받아오도록 요청함. 받아오면 회원 정보를 데이터베이스에 저장하고 JWT를 발급하고 쿠키에 저장한다. -> redirect 확인
+	// 위 로직은 e2e 테스트가 힘들기 때문에 구현을 먼저 하는 것으로 결정.
+
+	// #27 [04-04] 데이터베이스에서 로그인 데이터로 조회를 하여 비교한다.
+	// #28 [04-05] 없는 회원 정보라면 NotFoundError를 응답한다.
+	// #29 [04-06] 있는 회원 정보라면 JWT(Access Token 및 Refresh Token)를 발급하고 쿠키에 저장한다.
+	// #30 [04-07] JWT에 대한 Refresh Token을 Redis에 저장한다.
+	it.todo('POST /auth/signin');
+
+	// #33 [05-02] 로그아웃 요청을 받으면 토큰을 읽어 해당 회원의 로그인 여부를 확인한다.
+	// #34 [05-03] 로그인을 하지 않은 사용자의 요청이라면 BadRequest 에러를 반환한다.
+	// #35 [05-04] 로그인을 한 사용자라면 Redis의 Refresh Token 정보를 삭제한다.
+	// #36 [05-05] 브라우저 쿠키의 JWT를 없애는 요청을 보낸다.
+	it.todo('POST /auth/signout');
+});

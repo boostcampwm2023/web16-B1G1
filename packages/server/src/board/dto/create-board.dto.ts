aea5e66fa583a93176f1c4ab3c3bee9eec56202a
송준섭 <wnstjq7979@gmail.com>
2023-11-17 00:11:55 +0900

    :sparkles: Add Swagger Decorators on CreateBoardDto
    - CreateBoardDto에 Swagger 데코레이터 추가

--- packages/server/src/board/dto/create-board.dto.ts
+++ packages/server/src/board/dto/create-board.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBoardDto {
+	@ApiProperty({
+		description: '게시글 제목',
+		example: 'test title',
+		required: true,
+	})
 	title: string;
+
+	@ApiProperty({
+		description: '게시글 내용',
+		example: 'test content',
+		required: true,
+	})
 	content: string;
+
+	@ApiProperty({
+		description: '게시글 작성자',
+		example: 'test author',
+		required: true,
+	})
 	author: string;
 }

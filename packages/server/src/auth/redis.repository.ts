1fa5e9ba62e7c9e481a1f526743fe80bde213a5d
송준섭 <wnstjq7979@gmail.com>
2023-11-20 11:40:44 +0900

    :sparkles: Add RedisRepository
    - redis.repository.ts 추가

---
+++ packages/server/src/auth/redis.repository.ts
@@ -0,0 +1,20 @@
+import Redis from 'ioredis';
+import { Injectable } from '@nestjs/common';
+import { redisConfig } from 'src/config/redis.config';
+
+@Injectable()
+export class RedisRepository {
+	private readonly redisClient: Redis;
+
+	constructor() {
+		this.redisClient = new Redis(redisConfig);
+	}
+
+	async get(key: string) {
+		return this.redisClient.get(key);
+	}
+
+	async set(key: string, value: string) {
+		return this.redisClient.set(key, value);
+	}
+}

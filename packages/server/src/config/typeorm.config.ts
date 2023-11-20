d79cb363824f1199ff48265c4d6d88cf9959bbab
송준섭 <wnstjq7979@gmail.com>
2023-11-20 11:39:33 +0900

    :wrench: Add Redis Configuration
    - redis.config.ts 추가
    - .env에서 DB_HOST 등이었던 것들 MYSQL_HOST 등으로 수정

--- packages/server/src/config/typeorm.config.ts
+++ packages/server/src/config/typeorm.config.ts
@@ -3,12 +3,12 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-	type: process.env.DB_TYPE as any,
-	host: process.env.DB_HOST,
-	port: parseInt(process.env.DB_PORT),
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	type: process.env.MYSQL_TYPE as any,
+	host: process.env.MYSQL_HOST,
+	port: parseInt(process.env.MYSQL_PORT),
+	username: process.env.MYSQL_USERNAME,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
 	entities: [__dirname + '/../**/*.entity.{js,ts}'],
 	synchronize: true,
 };

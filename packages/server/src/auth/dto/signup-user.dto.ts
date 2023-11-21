bfae5d4ee4bfe0645a66e113e7e78b5a5175bc4c
송준섭 <wnstjq7979@gmail.com>
2023-11-21 12:34:34 +0900

    :sparkles: Add Class Validator on SignUpUserDto
    - username은 not null, string 타입, 최대 길이 50, 최소 길이 4, 영문과 숫자만 사용하도록 제약
    - password는 not null, string 타입, 최대 길이 100, 최소 길이 8로 제약
    - nickname은 not null, string 타입, 최대 길이 50, 최소 길이 4, 영문과 숫자, 한글만 사용하도록 제약

--- packages/server/src/auth/dto/signup-user.dto.ts
+++ packages/server/src/auth/dto/signup-user.dto.ts
@@ -1,10 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+	IsNotEmpty,
+	IsString,
+	Matches,
+	MaxLength,
+	MinLength,
+} from 'class-validator';
 
 export class SignUpUserDto {
 	@ApiProperty({
 		description: '로그인용 아이디',
 		example: 'test user',
 		required: true,
+	})
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(50)
+	@MinLength(4)
+	@Matches(/^[a-zA-Z0-9]+$/, {
+		message: '아이디는 영문자와 숫자만 사용할 수 있습니다.',
 	})
 	username: string;
 
@@ -13,12 +27,23 @@ export class SignUpUserDto {
 		example: 'test password',
 		required: true,
 	})
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(100)
+	@MinLength(8)
 	password: string;
 
 	@ApiProperty({
 		description: '닉네임',
 		example: 'test nickname',
 		required: true,
+	})
+	@IsNotEmpty()
+	@IsString()
+	@MaxLength(50)
+	@MinLength(2)
+	@Matches(/^[a-zA-Z0-9가-힣]+$/, {
+		message: '닉네임은 영문자, 숫자, 한글만 사용할 수 있습니다.',
 	})
 	nickname: string;
 }

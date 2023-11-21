077dc63683dada0194b85310b62949f47e92a43d
MinboyKim <kdm3801@gmail.com>
2023-11-21 14:09:10 +0900

    ✨ Implementing useFetch hook
    - useFetch 커스텀 훅 구현

---
+++ packages/client/src/shared/hooks/useFetch.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const BASE_URL = 'wow'; // import 해올 base url
+
+export const useFetch = (api: string) => {
+	const [data, setData] = useState<any>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<any>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axios.get(BASE_URL + api);
+				setData(res.data);
+				setLoading(false);
+			} catch (err) {
+				setError(err);
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [api]);
+
+	const refetch = async () => {
+		setLoading(true);
+		try {
+			const res = await axios.get(BASE_URL + api);
+			setData(res.data);
+			setLoading(false);
+		} catch (err) {
+			setError(err);
+			setLoading(false);
+		}
+	};
+
+	return { data, loading, error, refetch };
+};

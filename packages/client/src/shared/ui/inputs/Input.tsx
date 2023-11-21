581813e9d81c325e958e45ccab5f055dd359437f
MinboyKim <kdm3801@gmail.com>
2023-11-21 15:02:02 +0900

    ✨ Input component
    - 인풋 컴포넌트 구현

---
+++ packages/client/src/shared/ui/inputs/Input.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import theme from '../styles/theme';
+
+const Input = styled.input`
+	width: 452px;
+	height: 43px;
+	padding: 12px;
+	border-radius: 4px;
+	background-color: ${theme.colors.background['bdp-03']};
+	border: 1px solid ${theme.colors.stroke.default};
+	color: ${theme.colors.text.third};
+
+	&:hover {
+		border: 1px solid ${theme.colors.stroke.sc};
+	}
+
+	&:focus {
+		outline: none;
+		border: 1px solid ${theme.colors.stroke.focus};
+	}
+`;
+
+const Label = styled.label`
+	margin-bottom: 8px;
+	color: ${theme.colors.text.secondary};
+`;
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+interface PropsType extends React.InputHTMLAttributes<HTMLInputElement> {
+	label: string;
+	id: string;
+	placeholder: string;
+}
+
+export default function InputBar({
+	id,
+	label,
+	placeholder,
+	...rest
+}: PropsType) {
+	return (
+		<Container>
+			<Label htmlFor={id}>{label}</Label>
+			<Input id={id} placeholder={placeholder} {...rest} />
+		</Container>
+	);
+}

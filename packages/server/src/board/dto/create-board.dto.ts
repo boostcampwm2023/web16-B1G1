0652b8c44a20db6e11560b0647447e65c6895cfa
qkrwogk <pjha999@naver.com>
2023-11-22 19:56:34 +0900

    ✨ remove author on create board dto
    - 개선 완료하여 author 입력 삭제

--- packages/server/src/board/dto/create-board.dto.ts
+++ packages/server/src/board/dto/create-board.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, Max, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateBoardDto {
 	@IsNotEmpty({ message: '게시글 제목은 필수 입력입니다.' })
@@ -20,15 +20,4 @@ export class CreateBoardDto {
 		required: true,
 	})
 	content: string;
-
-	// 서버에서 직접 삽입해주도록 변경 (validation 제거)
-	// @IsNotEmpty({ message: '게시글 작성자는 필수 입력입니다.' })
-	// @IsString({ message: '게시글 작성자는 문자열로 입력해야 합니다.' })
-	// @MaxLength(50, { message: '게시글 작성자는 50자 이내로 입력해야 합니다.' })
-	// @ApiProperty({
-	// 	description: '게시글 작성자',
-	// 	example: 'test author',
-	// 	required: true,
-	// })
-	author: string;
 }

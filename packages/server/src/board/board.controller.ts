279968b721fc460484bbdb288a8fa78ec85e84cb
qkrwogk <pjha999@naver.com>
2023-11-23 00:15:40 +0900

    ✨ integrate POST /board & image, receive multiple
    - 이미지 업로드를 위한 컨트롤러 메소드 삭제 및 POST /board에 통합
    - 복수의 파일 업로드(3개까지) 지원하도록 변경
    - CreateBoardDTO 제거하고 Express.Multer.File로 대체
    - 로컬 폴더에 저장 삭제

--- packages/server/src/board/board.controller.ts
+++ packages/server/src/board/board.controller.ts
@@ -8,11 +8,11 @@ import {
 	Delete,
 	Query,
 	UseInterceptors,
-	UploadedFile,
 	UsePipes,
 	ValidationPipe,
 	ParseIntPipe,
 	UseGuards,
+	UploadedFiles,
 } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,11 +24,9 @@ import {
 	ApiNotFoundResponse,
 	ApiOkResponse,
 	ApiOperation,
-	ApiParam,
 	ApiTags,
 } from '@nestjs/swagger';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { CreateImageDto } from './dto/create-image.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { CookieAuthGuard } from 'src/auth/cookie-auth.guard';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { UserDataDto } from './dto/user-data.dto';
@@ -41,6 +39,7 @@ export class BoardController {
 
 	@Post()
 	@UseGuards(CookieAuthGuard)
+	@UseInterceptors(FilesInterceptor('file', 3))
 	@UsePipes(ValidationPipe)
 	@ApiOperation({ summary: '게시글 작성', description: '게시글을 작성합니다.' })
 	@ApiCreatedResponse({ status: 201, description: '게시글 작성 성공' })
@@ -51,8 +50,9 @@ export class BoardController {
 	createBoard(
 		@Body() createBoardDto: CreateBoardDto,
 		@GetUser() userData: UserDataDto,
+		@UploadedFiles() files: Express.Multer.File[],
 	): Promise<Board> {
-		return this.boardService.createBoard(createBoardDto, userData);
+		return this.boardService.createBoard(createBoardDto, userData, files);
 	}
 
 	@Get()
@@ -176,27 +176,5 @@ export class BoardController {
 		@GetUser() userData: UserDataDto,
 	): Promise<void> {
 		return this.boardService.deleteBoard(id, userData);
-	}
-
-	@Post(':id/image')
-	@UseGuards(CookieAuthGuard)
-	@UseInterceptors(FileInterceptor('file', { dest: './uploads' }))
-	@UsePipes(ValidationPipe)
-	@ApiOperation({
-		summary: '이미지 파일 업로드',
-		description: '이미지 파일을 업로드합니다.',
-	})
-	@ApiParam({ name: 'id', description: '게시글 번호' })
-	@ApiOkResponse({ status: 200, description: '이미지 파일 업로드 성공' })
-	@ApiBadRequestResponse({
-		status: 400,
-		description: '잘못된 요청으로 파일 업로드 실패',
-	})
-	@ApiNotFoundResponse({ status: 404, description: '게시글이 존재하지 않음' })
-	uploadFile(
-		@Param('id', ParseIntPipe) board_id: number,
-		@UploadedFile() file: CreateImageDto,
-	): Promise<Board> {
-		return this.boardService.uploadFile(board_id, file);
 	}
 }

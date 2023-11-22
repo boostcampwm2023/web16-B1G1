e4f526e1432368fb10a5a69f4bf6a49f000cdf82
qkrwogk <pjha999@naver.com>
2023-11-22 12:10:33 +0900

    ⚡️ append crypto on board content
    - 게시글 DB 삽입 시 본문 암호화하여 저장 (생성, 수정 단계)
    - 게시글 DB 조회 시 본문 복호화하여 반환 (조회 단계)

--- packages/server/src/board/board.service.ts
+++ packages/server/src/board/board.service.ts
@@ -12,6 +12,7 @@ import { Repository } from 'typeorm';
 import { unlinkSync } from 'fs';
 import { CreateImageDto } from './dto/create-image.dto';
 import { Image } from './entities/image.entity';
+import { encryptAes, decryptAes } from 'src/utils/aes.util';
 
 @Injectable()
 export class BoardService {
@@ -27,7 +28,7 @@ export class BoardService {
 
 		const board = this.boardRepository.create({
 			title,
-			content,
+			content: encryptAes(content), // AES 암호화하여 저장
 			author,
 		});
 		const created: Board = await this.boardRepository.save(board);
@@ -50,11 +51,19 @@ export class BoardService {
 		if (!found) {
 			throw new NotFoundException(`Not found board with id: ${id}`);
 		}
+		if (found.content) {
+			found.content = decryptAes(found.content); // AES 복호화하여 반환
+		}
 		return found;
 	}
 
 	async updateBoard(id: number, updateBoardDto: UpdateBoardDto) {
 		const board: Board = await this.findBoardById(id);
+
+		// updateBoardDto.content가 존재하면 AES 암호화하여 저장
+		if (updateBoardDto.content) {
+			updateBoardDto.content = encryptAes(updateBoardDto.content);
+		}
 
 		const updatedBoard: Board = await this.boardRepository.save({
 			...board,

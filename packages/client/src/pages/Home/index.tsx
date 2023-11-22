333e59163e65a8d23cf8f53dc193e6a6f2916c69
KimGaeun <greencloud11@hanyang.ac.kr>
2023-11-22 18:10:31 +0900

    ⚡️ Change setTImeout to onAnimationEnd
    - Toast의 setTimeout을 onAnimationEnd로 변경하여 애니메이션이 끝나면 null을 반환하도록 함
    - text에 whitespace:nowrap을 적용하여 창이 줄어들어도 찌그러지지 않게 함

--- packages/client/src/pages/Home/index.tsx
+++ packages/client/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import Toast from 'shared/ui/toast/Toast';
 import Screen from 'widgets/screen';
 
 export default function Home() {
-	return <Screen />;
+	// return <Screen />;
+	return <Toast>토스트입니다</Toast>;
 }

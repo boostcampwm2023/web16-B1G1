c076be40d8e4152bccc8f20398b66a2bc4b2f9b1
KimGaeun <greencloud11@hanyang.ac.kr>
2023-11-22 21:54:31 +0900

    ✨ Add logic to SignUpModal
    - 모든 input이 채워지면 버튼이 활성화됨

--- packages/client/src/widgets/signupModal/ui/SignUpModal.tsx
+++ packages/client/src/widgets/signupModal/ui/SignUpModal.tsx
@@ -3,26 +3,66 @@ import { Button, Modal } from 'shared/ui';
 import InputBar from 'shared/ui/inputBar/InputBar';
 import { useState } from 'react';
 
-export default function SignupModal() {
+export default function SignUpModal() {
 	const [isAllInputFilled, setIsAllInputFilled] = useState(false);
 
+	const [inputValues, setInputValues] = useState({
+		id: '',
+		password: '',
+		checkPassword: '',
+	});
+
+	const handleGoBackButton = () => {
+		// TODO: 로그인 모달로 이동하도록 하기
+	};
+
+	const handleSignUpButton = () => {
+		if (!isAllInputFilled) return;
+
+		console.log(inputValues);
+		// TODO: 회원가입 요청 보낸 후 로그인 모달로 이동
+	};
+
+	const handleInputs = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, value } = target;
+
+		setInputValues((prev) => {
+			const updatedValues = { ...prev, [id]: value };
+
+			if (
+				updatedValues.id &&
+				updatedValues.password &&
+				updatedValues.checkPassword
+			) {
+				setIsAllInputFilled(true);
+				return updatedValues;
+			}
+
+			setIsAllInputFilled(false);
+			return updatedValues;
+		});
+	};
+
 	const signupButton = isAllInputFilled ? (
-		<Button onClick={() => {}} buttonType="CTA-icon" size="m">
+		<Button onClick={handleSignUpButton} buttonType="CTA-icon" size="m">
 			회원가입
 		</Button>
 	) : (
-		<Button onClick={() => {}} buttonType="CTA-icon" size="m" disabled>
+		<Button
+			onClick={handleSignUpButton}
+			buttonType="CTA-icon"
+			size="m"
+			disabled
+		>
 			회원가입
 		</Button>
 	);
-
-	const onClickGoBack = () => {};
 
 	return (
 		<Modal
 			title="회원가입"
 			rightButton={signupButton}
-			onClickGoBack={onClickGoBack}
+			onClickGoBack={handleGoBackButton}
 		>
 			<InputBarsContainer>
 				<InputBar
@@ -30,18 +70,21 @@ export default function SignupModal() {
 					label="아이디"
 					placeholder="아이디를 입력해주세요."
 					isEssential
+					onChange={handleInputs}
 				/>
 				<InputBar
 					id="password"
 					label="비밀번호"
 					placeholder="비밀번호를 입력해주세요."
 					isEssential
+					onChange={handleInputs}
 				/>
 				<InputBar
-					id="check-password"
+					id="checkPassword"
 					label="비밀번호 확인"
 					placeholder="비밀번호를 다시 입력해주세요."
 					isEssential
+					onChange={handleInputs}
 				/>
 			</InputBarsContainer>
 		</Modal>

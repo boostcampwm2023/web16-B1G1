ee5ad3eda746b43d5469d7fd049db3e54619d4e3
bananaba <200tiger1@gmail.com>
2023-11-23 22:49:01 +0900

    ✨ Landing Page camera movement
    - 랜딩 페이지 에서의 카메라 움직임을 구현했다

--- packages/client/src/pages/Landing/index.tsx
+++ packages/client/src/pages/Landing/index.tsx
@@ -2,7 +2,7 @@ import LandingScreen from 'widgets/landingScreen';
 import LoginModal from 'widgets/loginModal';
 import LogoAndStart from 'widgets/logoAndStart';
 import SignUpModal from 'widgets/signupModal/SignUpModal';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 function pageReducer(state: number, action: { type: 'NEXT' | 'PREV' }) {
 	switch (action.type) {
@@ -17,12 +17,21 @@ function pageReducer(state: number, action: { type: 'NEXT' | 'PREV' }) {
 
 export default function Landing() {
 	const [page, dispatch] = useReducer(pageReducer, 0);
+	const [mouse, setMouse] = useState([0.5, 0.5]);
+
 	return (
-		<>
+		<div
+			onMouseMove={(e) => {
+				setMouse([
+					e.clientX / window.innerWidth,
+					e.clientY / window.innerHeight,
+				]);
+			}}
+		>
 			{page === 0 && <LogoAndStart changePage={dispatch} />}
 			{page === 1 && <LoginModal changePage={dispatch} />}
 			{page === 2 && <SignUpModal changePage={dispatch} />}
-			<LandingScreen />
-		</>
+			<LandingScreen mousePosition={mouse} />
+		</div>
 	);
 }

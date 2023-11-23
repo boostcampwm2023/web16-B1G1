af1ac358d55ac482debaa0a88d22fcdcfacc088e
KimGaeun <greencloud11@hanyang.ac.kr>
2023-11-23 13:06:56 +0900

    🐛 Resolve git conflict

--- packages/client/src/shared/ui/buttons/index.ts
+++ packages/client/src/shared/ui/buttons/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 export { default as Button } from './Button';
 export { default as TextButton } from './TextButton';
 export { default as IconButton } from './IconButton';
-=======
-export { default as Button } from './button';
-export { default as TextButton } from './textButton';
-export { default as IconButton } from './iconButton';
->>>>>>> e49fbdc63c7f2a0ae2c76c913848ca2696116712

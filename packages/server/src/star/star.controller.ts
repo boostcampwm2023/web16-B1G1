b05819beeb7e03009c01b62c1b801078d7f81ed5
qkrwogk <pjha999@naver.com>
2023-11-28 00:38:37 +0900

    ✨ implement GET /star
    - module엔 AuthModule, TypeORMModule(Board), MongooseModule(Star) 등록
    - controller엔 다음을 구현
      - by-author 파라미터로 별 목록을 리턴하는 findAllStarsByAuthor
      - Auth Guard로 user data를 얻어 자신의 별 목록을 리턴하는 findAllStarsMine
    - service엔 다음을 구현
      - author에 따른 글 목록을 받고, star id를 이용해 MongoDB에서 별 정보를 받아 양식에 맞게 별 목록을 리턴

--- packages/server/src/star/star.controller.ts
+++ packages/server/src/star/star.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	UseGuards,
+	Query,
+} from '@nestjs/common';
 import { StarService } from './star.service';
 import { CreateStarDto } from './dto/create-star.dto';
 import { UpdateStarDto } from './dto/update-star.dto';
+import { CookieAuthGuard } from 'src/auth/cookie-auth.guard';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { UserDataDto } from 'src/auth/dto/user-data.dto';
+import { GetStarResDto } from './dto/get-star-res.dto';
 
 @Controller('star')
 export class StarController {
-  constructor(private readonly starService: StarService) {}
+	constructor(private readonly starService: StarService) {}
 
-  @Post()
-  create(@Body() createStarDto: CreateStarDto) {
-    return this.starService.create(createStarDto);
-  }
+	@Post()
+	create(@Body() createStarDto: CreateStarDto) {
+		return this.starService.create(createStarDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.starService.findAll();
-  }
+	@Get()
+	@UseGuards(CookieAuthGuard)
+	findAllStarsMine(@GetUser() userData: UserDataDto): Promise<GetStarResDto[]> {
+		const author = userData.nickname;
+		return this.starService.findAllStarsByAuthor(author);
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.starService.findOne(+id);
-  }
+	@Get('by-author')
+	findAllStarsByAuthor(
+		@Query('author') author: string,
+	): Promise<GetStarResDto[]> {
+		return this.starService.findAllStarsByAuthor(author);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStarDto: UpdateStarDto) {
-    return this.starService.update(+id, updateStarDto);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.starService.findOne(+id);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.starService.remove(+id);
-  }
+	@Patch(':id')
+	update(@Param('id') id: string, @Body() updateStarDto: UpdateStarDto) {
+		return this.starService.update(+id, updateStarDto);
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.starService.remove(+id);
+	}
 }
--- packages/server/src/star/star.service.ts
+++ packages/server/src/star/star.service.ts
@@ -1,26 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateStarDto } from './dto/create-star.dto';
 import { UpdateStarDto } from './dto/update-star.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from 'src/board/entities/board.entity';
+import { Repository } from 'typeorm';
+import { InjectModel } from '@nestjs/mongoose';
+import { Star } from './schemas/star.schema';
+import { Model } from 'mongoose';
+import { GetStarResDto } from './dto/get-star-res.dto';
 
 @Injectable()
 export class StarService {
-  create(createStarDto: CreateStarDto) {
-    return 'This action adds a new star';
-  }
+	constructor(
+		@InjectRepository(Board)
+		private readonly boardRepository: Repository<Board>,
+		@InjectModel(Star.name)
+		private readonly starModel: Model<Star>,
+	) {}
 
-  findAll() {
-    return `This action returns all star`;
-  }
+	create(createStarDto: CreateStarDto) {
+		return 'This action adds a new star';
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} star`;
-  }
+	async findAllStarsByAuthor(author: string): Promise<GetStarResDto[]> {
+		// author가 없는 경우 에러 발생
+		if (!author) {
+			throw new BadRequestException('author is required');
+		}
 
-  update(id: number, updateStarDto: UpdateStarDto) {
-    return `This action updates a #${id} star`;
-  }
+		const boards = await this.boardRepository.findBy({
+			user: { nickname: author },
+		});
 
-  remove(id: number) {
-    return `This action removes a #${id} star`;
-  }
+		const starDataList: GetStarResDto[] = [];
+		for (let board of boards) {
+			if (!board.star) continue;
+
+			const star = await this.starModel.findOne({ _id: board.star });
+			if (!star) continue;
+
+			// $, _로 시작하는 모든 속성 제거
+			for (let key in star) {
+				if (key.startsWith('$') || key.startsWith('_')) {
+					delete star[key];
+				}
+			}
+
+			starDataList.push({
+				id: board.id,
+				...star,
+				title: board.title,
+			});
+		}
+
+		return starDataList;
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} star`;
+	}
+
+	update(id: number, updateStarDto: UpdateStarDto) {
+		return `This action updates a #${id} star`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} star`;
+	}
 }

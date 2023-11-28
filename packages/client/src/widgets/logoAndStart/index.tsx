39d052f2878a8fac795284d47fbd8f937486d15f
bananaba <200tiger1@gmail.com>
2023-11-29 03:13:19 +0900

    ✨ Login user go home
    - 이미 로그인한 사용자(토큰이 만료되지 않은 사용자)의 경우 로그인 과정을 생략하고 홈으로 가도록 했다

--- packages/client/src/widgets/logoAndStart/index.tsx
+++ packages/client/src/widgets/logoAndStart/index.tsx
@@ -2,15 +2,23 @@ import { Button } from 'shared/ui';
 import styled from '@emotion/styled';
 import { Title02 } from 'shared/ui/styles';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 export default function LogoAndStart() {
 	const navigate = useNavigate();
+
+	const handleStart = () => {
+		const accessToken = Cookies.get('accessToken');
+		const refreshToken = Cookies.get('refreshToken');
+		if (!accessToken || !refreshToken) navigate('/login');
+		else navigate('/home');
+	};
 
 	return (
 		<Container>
 			<Logo>Logo</Logo>
 			<Button
-				onClick={() => navigate('/login')}
+				onClick={handleStart}
 				size="l"
 				buttonType="CTA-icon"
 				type="button"

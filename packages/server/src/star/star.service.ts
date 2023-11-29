90bd4bf15428987757b4b15446951c5164ce4148
qkrwogk <pjha999@naver.com>
2023-11-29 16:39:03 +0900

    ✨ added PATCH /star/:id
    - 게시글 id를 이용하여 해당 별글의 별 스타일을 변경하는 API 구현
    - 속성을 정의하여 제약을 두지 않고 Object DB의 장점을 살려 프론트에서 필요에 따라 속성을 마음대로 추가할 수 있게 구성

--- packages/server/src/star/star.service.ts
+++ packages/server/src/star/star.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+} from '@nestjs/common';
 import { UpdateStarDto } from './dto/update-star.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from '../board/entities/board.entity';
@@ -7,6 +11,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Star } from './schemas/star.schema';
 import { Model } from 'mongoose';
 import { GetStarResDto } from './dto/get-star-res.dto';
+import { UserDataDto } from 'src/auth/dto/user-data.dto';
 
 @Injectable()
 export class StarService {
@@ -55,7 +60,50 @@ export class StarService {
 		return starDataList;
 	}
 
-	updateStarByPostId(post_id: number, updateStarDto: UpdateStarDto) {
-		return `This action updates post #${post_id}'s star`;
+	async updateStarByPostId(
+		post_id: number,
+		updateStarDto: UpdateStarDto,
+		userData: UserDataDto,
+	): Promise<Star> {
+		const board: Board = await this.boardRepository.findOneBy({ id: post_id });
+		if (!board) {
+			throw new BadRequestException(`Not found board with id: ${post_id}`);
+		}
+
+		// 게시글 작성자와 수정 요청자가 다른 경우
+		if (board.user.id !== userData.userId) {
+			throw new BadRequestException('You are not the author of this star');
+		}
+
+		// 별 id를 조회하여 없으면 에러 리턴
+		const star_id = board.star;
+		if (!star_id) {
+			throw new BadRequestException(
+				`Not found star_id with this post_id: ${post_id}`,
+			);
+		}
+
+		// 별 스타일이 존재하면 MongoDB에 저장
+		const result = await this.starModel.updateOne(
+			{ _id: star_id },
+			{ ...updateStarDto },
+		);
+		if (!result) {
+			throw new InternalServerErrorException(
+				`Failed to update star with this post_id: ${post_id}, star_id: ${star_id}`,
+			);
+		} else if (result.matchedCount === 0) {
+			throw new BadRequestException(
+				`Not found star with this post_id: ${post_id}, star_id: ${star_id}`,
+			);
+		} else if (result.modifiedCount === 0) {
+			throw new BadRequestException(`Nothing to update`);
+		}
+
+		const updatedStar = await this.starModel.findOne({
+			_id: star_id,
+		});
+
+		return updatedStar;
 	}
 }

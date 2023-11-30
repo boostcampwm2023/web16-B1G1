99a25c14b2cbc0936e835da69e10df965782f368
송준섭 <wnstjq7979@gmail.com>
2023-11-30 13:57:54 +0900

    :sparkles: Add GetQuerryRunner Decorator
    - req에서 querryrunner를 가져오는 GetQuerryRunner 데코레이터 추가

---
+++ packages/server/src/interceptor/decorators/get-querry-runner.decorator.ts
@@ -0,0 +1,21 @@
+import {
+	ExecutionContext,
+	createParamDecorator,
+	InternalServerErrorException,
+} from '@nestjs/common';
+import { QueryRunner } from 'typeorm';
+
+export const GetQuerryRunner = createParamDecorator(
+	(data: keyof QueryRunner | undefined, context: ExecutionContext) => {
+		const req = context.switchToHttp().getRequest();
+
+		const queryRunner = req.queryRunner as QueryRunner;
+		if (!queryRunner) {
+			throw new InternalServerErrorException(
+				'TransactionInterceptor를 적용해야 @QuerryRunner를 사용할 수 있습니다.',
+			);
+		}
+
+		return queryRunner;
+	},
+);

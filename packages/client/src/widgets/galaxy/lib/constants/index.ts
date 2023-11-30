ddec4d68187515c4868f2d05b8fb401d5b063acc
bananaba <200tiger1@gmail.com>
2023-11-30 10:47:42 +0900

    🎨 Logarithmic spiral
    - 로그 스파이럴을 적용한 스파이럴 함수를 만들었다

--- packages/client/src/widgets/galaxy/lib/constants/index.ts
+++ packages/client/src/widgets/galaxy/lib/constants/index.ts
@@ -14,3 +14,9 @@ export const starTypes = {
 	color: [0xffcc6f, 0xffd2a1, 0xfff4ea, 0xf8f7ff, 0xcad7ff, 0xaabfff],
 	size: [0.7, 0.7, 1.15, 1.48, 2.0, 2.5, 3.5],
 };
+
+// export const ARMS_X_DIST = 5000;
+// export const ARMS_Z_DIST = 1000;
+// export const GALAXY_THICKNESS = 300;
+// export const SPIRAL = 1.2;
+// export const SPIRAL_START = 1000;

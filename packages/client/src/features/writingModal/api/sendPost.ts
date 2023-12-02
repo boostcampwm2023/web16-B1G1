579f9d467fbd19f0affa61eb4dd54b34cea0f767
MinboyKim <kdm3801@gmail.com>
2023-12-02 17:09:14 +0900

    🐛 Empty title and content
    - 빈 제목 및 내용 디폴트 값 설정

--- packages/client/src/features/writingModal/api/sendPost.ts
+++ packages/client/src/features/writingModal/api/sendPost.ts
@@ -18,8 +18,8 @@ export const sendPost = async (
 ) => {
 	try {
 		const formData = new FormData();
-		formData.append('title', title);
-		formData.append('content', content);
+		formData.append('title', title !== '' ? title : '제목 없음');
+		formData.append('content', content !== '' ? content : '내용 없음');
 		formData.append('star', JSON.stringify(dummyStarData));
 		if (files) {
 			for (let i = 0; i < files.length; i++) formData.append('file', files[i]);

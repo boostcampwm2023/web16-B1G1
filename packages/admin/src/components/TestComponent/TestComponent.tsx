2b600d27423d76520cde8a8f6b3b1b52030ca49f
qkrwogk <pjha999@naver.com>
2023-12-04 21:05:40 +0900

    ✨ add navbar and test component
    - nav바와 test component 작성
    - 앞으로 admin 기능 추가 시 틀 작성
    
    Co-authored-by: 송준섭 <wnstjq7979@gmail.com>

---
+++ packages/admin/src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,7 @@
+export const TestComponent = () => {
+	return (
+		<div>
+			<h1>Test Component</h1>
+		</div>
+	);
+};
--- packages/admin/src/main.tsx
+++ packages/admin/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+);

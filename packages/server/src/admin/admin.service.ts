a1f69bd072f4cbe38703cb74ddb98426587d6c2a
qkrwogk <pjha999@naver.com>
2023-12-04 22:08:06 +0900

    ✨ import typeorm module on admin module
    - User, Board 리포지토리 삽입
    
    Co-authored-by: 송준섭 <wnstjq7979@gmail.com>

--- packages/server/src/admin/admin.service.ts
+++ packages/server/src/admin/admin.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Board } from 'src/board/entities/board.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+	constructor(
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>,
+		@InjectRepository(Board)
+		private readonly boardRepository: Repository<Board>,
+	) {}
+}

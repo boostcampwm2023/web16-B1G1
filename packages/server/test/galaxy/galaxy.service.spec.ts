3d96b95387aaebb9d2393bd209ddd1946d7fa15a
qkrwogk <pjha999@naver.com>
2023-12-04 14:04:45 +0900

    ♻️ move test files to test folder
    - 테스트 파일은 테스트 폴더에

---
+++ packages/server/test/galaxy/galaxy.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { GalaxyService } from '../../src/galaxy/galaxy.service';
+
+describe('GalaxyService', () => {
+	let service: GalaxyService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [GalaxyService],
+		}).compile();
+
+		service = module.get<GalaxyService>(GalaxyService);
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+});

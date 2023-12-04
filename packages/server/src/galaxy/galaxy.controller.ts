6467abc5526d73cf638315030147c644bbca2bdf
qkrwogk <pjha999@naver.com>
2023-12-04 14:08:26 +0900

    ✨ append logger and swagger on galaxy controller
    - 로거 인터셉터와 swagger api tag 컨트롤러에 등록

--- packages/server/src/galaxy/galaxy.controller.ts
+++ packages/server/src/galaxy/galaxy.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	UseInterceptors,
+} from '@nestjs/common';
 import { GalaxyService } from './galaxy.service';
 import { CreateGalaxyDto } from './dto/create-galaxy.dto';
 import { UpdateGalaxyDto } from './dto/update-galaxy.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { LogInterceptor } from 'src/interceptor/log.interceptor';
 
 @Controller('galaxy')
+@UseInterceptors(LogInterceptor)
+@ApiTags('은하 API')
 export class GalaxyController {
-  constructor(private readonly galaxyService: GalaxyService) {}
+	constructor(private readonly galaxyService: GalaxyService) {}
 
-  @Post()
-  create(@Body() createGalaxyDto: CreateGalaxyDto) {
-    return this.galaxyService.create(createGalaxyDto);
-  }
+	@Post()
+	create(@Body() createGalaxyDto: CreateGalaxyDto) {
+		return this.galaxyService.create(createGalaxyDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.galaxyService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.galaxyService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.galaxyService.findOne(+id);
-  }
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.galaxyService.findOne(+id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGalaxyDto: UpdateGalaxyDto) {
-    return this.galaxyService.update(+id, updateGalaxyDto);
-  }
+	@Patch(':id')
+	update(@Param('id') id: string, @Body() updateGalaxyDto: UpdateGalaxyDto) {
+		return this.galaxyService.update(+id, updateGalaxyDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.galaxyService.remove(+id);
-  }
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.galaxyService.remove(+id);
+	}
 }

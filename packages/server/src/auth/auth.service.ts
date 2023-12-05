04ba77346a17a44c22b9606d2c02a13bd41d6cd7
송준섭 <wnstjq7979@gmail.com>
2023-12-05 15:00:35 +0900

    :sparkles: Update Sharelink Api
    - user.status가 private이어도 상관 없이 링크를 조회하도록 수정

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -244,8 +244,8 @@ export class AuthService {
 	async getShareLinkByNickname(nickname: string) {
 		const user = await this.userRepository.findOneBy({ nickname });
 
-		if (user.status === UserShareStatus.PRIVATE) {
-			throw new UnauthorizedException('비공개 상태입니다.');
+		if (!user) {
+			throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
 		}
 
 		const foundLink = await this.shareLinkRepository.findOneBy({
@@ -283,10 +283,6 @@ export class AuthService {
 			throw new InternalServerErrorException(
 				'링크에 대한 사용자가 존재하지 않습니다.',
 			);
-		}
-
-		if (linkUser.status === UserShareStatus.PRIVATE) {
-			throw new UnauthorizedException('비공개 상태입니다.');
 		}
 
 		return linkUser.username;

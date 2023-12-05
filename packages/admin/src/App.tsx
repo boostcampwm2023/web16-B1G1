bb8733f55c5eb5a0fb451517aadc1efd0d2c5a46
qkrwogk <pjha999@naver.com>
2023-12-05 13:25:21 +0900

    ✨ refactor & style nav, append board menu
    - Nav 스타일링, App.tsx에서 내용 수정 가능하도록 리팩토링
    - 게시글 조회 메뉴 등록

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { TestComponent } from './components/TestComponent/TestComponent.tsx';
-import { Nav } from './components/Nav.tsx';
+
+import Board from './components/Board/Board.tsx';
+import Nav from './components/Nav.tsx';
 
 function App() {
 	return (
 		<>
-			<Nav />
+			<Nav>
+				<a href="/">Home</a>
+				<a href="/about">About</a>
+				<a href="/abc">Test</a>
+				<a href="/board">Board</a>
+			</Nav>
 			<Routes>
 				<Route path="/" element={<div>Home</div>} />
 				<Route path="/about" element={<div>About</div>} />
 				<Route path="/abc" element={<TestComponent />} />
+				<Route path="/board" element={<Board />} />
 			</Routes>
 		</>
 	);

fe17beb44125eb205db6cb7da7e5d34e225d3204
송준섭 <wnstjq7979@gmail.com>
2023-12-06 15:25:39 +0900

    :sparkles: Add Exception Statistics Component
    - Exception Statistics 컴포넌트 추가
    - App.tsx에 라우트 연결

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Board from './components/Board/Board.tsx';
 import Nav from './components/Nav.tsx';
 import SystemInfo from './components/SystemInfo/SystemInfo.tsx';
+import ExceptionStatistics from './components/ExceptionStatistics/ExceptionStatistics.tsx';
 
 function App() {
 	return (
@@ -12,11 +13,16 @@ function App() {
 				<a href="/admin">Home</a>
 				<a href="/admin/board">Board</a>
 				<a href="/admin/system-info">System Info</a>
+				<a href={'/admin/exception-statistics'}>Exception Statistics</a>
 			</Nav>
 			<Routes>
 				<Route path="/admin" element={<div>Home</div>} />
 				<Route path="/admin/board" element={<Board />} />
 				<Route path="/admin/system-info" element={<SystemInfo />} />
+				<Route
+					path={'/admin/exception-statistics'}
+					element={<ExceptionStatistics />}
+				/>
 			</Routes>
 		</>
 	);
---
+++ packages/admin/src/components/ExceptionStatistics/ExceptionStatistics.tsx
@@ -0,0 +1,3 @@
+export default function ExceptionStatistics() {
+	return <div>ExceptionStatistics</div>;
+}

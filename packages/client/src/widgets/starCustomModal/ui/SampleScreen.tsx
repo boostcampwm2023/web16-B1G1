8ff08423f3c1cf2ee9ca38d8d6ae1a54749442cf
KimGaeun <greencloud11@hanyang.ac.kr>
2023-12-06 17:38:19 +0900

    ✨ Add SampleScreen to StarCustomModal
    - 변경되는 스타일을 실시간으로 반영하는 화면 추가

---
+++ packages/client/src/widgets/starCustomModal/ui/SampleScreen.tsx
@@ -0,0 +1,105 @@
+import { Canvas } from '@react-three/fiber';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
+import Star from 'features/star';
+import * as THREE from 'three';
+import { OrbitControls } from '@react-three/drei';
+import { shapeTypes } from '@constants';
+import leftArrowIcon from '@icons/icon-arrow-left-32-white.svg';
+import rightArrowIcon from '@icons/icon-arrow-right-32-white.svg';
+import styled from '@emotion/styled';
+
+interface PropsType {
+	shape: number;
+	setShape: React.Dispatch<React.SetStateAction<number>>;
+	color: string;
+	size: number;
+	brightness: number;
+}
+
+export default function SampleScreen({
+	shape,
+	setShape,
+	color,
+	size,
+	brightness,
+}: PropsType) {
+	const changeShapePrevious = () => {
+		if (shape === 0) {
+			setShape(shapeTypes.length - 1);
+			return;
+		}
+		setShape(shape - 1);
+	};
+
+	const changeShapeNext = () => {
+		if (shape === shapeTypes.length - 1) {
+			setShape(0);
+			return;
+		}
+		setShape(shape + 1);
+	};
+
+	return (
+		<Layout>
+			<LeftArrowIcon
+				src={leftArrowIcon}
+				alt="이전 별 모양 보기"
+				height={35}
+				width={35}
+				onClick={changeShapePrevious}
+			/>
+			<RightArrowIcon
+				src={rightArrowIcon}
+				alt="다음 별 모양 보기"
+				height={35}
+				width={35}
+				onClick={changeShapeNext}
+			/>
+
+			<Canvas camera={{ position: [0, 0, 750], far: 10000 }}>
+				<EffectComposer>
+					<Bloom
+						intensity={0.4}
+						mipmapBlur={false}
+						luminanceThreshold={0.9}
+						luminanceSmoothing={0.025}
+					/>
+				</EffectComposer>
+
+				<color attach="background" args={['#070614']} />
+
+				<directionalLight color={'#fff'} intensity={4} />
+				<ambientLight color="#fff" intensity={1} />
+
+				<Star
+					position={new THREE.Vector3(0, 0, 0)}
+					size={size}
+					color={color}
+					shape={shapeTypes[shape]}
+					brightness={brightness}
+				/>
+
+				<OrbitControls enableZoom={false} />
+			</Canvas>
+		</Layout>
+	);
+}
+
+const Layout = styled.div`
+	display: flex;
+	position: relative;
+	height: 400px;
+	width: 400px;
+`;
+
+const LeftArrowIcon = styled.img`
+	position: absolute;
+	z-index: 20;
+	top: 50%;
+	transform: translateY(-50%);
+	cursor: pointer;
+`;
+
+const RightArrowIcon = styled(LeftArrowIcon)`
+	right: 0;
+`;

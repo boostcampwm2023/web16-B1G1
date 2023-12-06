d580f31cce5d0747c33193157dee3f56e4187e71
KimGaeun <greencloud11@hanyang.ac.kr>
2023-12-06 17:43:19 +0900

    🎨 Seperate constants related to star custom

---
+++ packages/client/src/widgets/starCustomModal/lib/constants.ts
@@ -0,0 +1,13 @@
+export const STAR_MIN_SIZE = 100;
+export const STAR_MAX_SIZE = 300;
+export const STAR_DEFAULT_SIZE = 200;
+export const STAR_SIZE_STEP = 10;
+
+export const STAR_MIN_BRIGHTNESS = -1;
+export const STAR_MAX_BRIGHTNESS = 4;
+export const STAR_DEFAULT_BRIGHTNESS = 0;
+export const STAR_BRIGHTNESS_STEP = 0.2;
+
+export const STAR_DEFAULT_COLOR = '#b3b7e6';
+
+export const STAR_DEFAULT_SHAPE_INDEX = 0;

85226420ca1559755e6ff4feff905427e3775171
송준섭 <wnstjq7979@gmail.com>
2023-12-06 15:56:58 +0900

    :sparkles: Add getExceptions Function in ExceptionStatistics Component
    - ExceptionStatistics 컴포넌트 렌더링시 exception 정보를 가져와서 렌더링하도록 구현

--- packages/admin/src/components/ExceptionStatistics/ExceptionStatistics.tsx
+++ packages/admin/src/components/ExceptionStatistics/ExceptionStatistics.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from 'react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 export default function ExceptionStatistics() {
-	return <div>ExceptionStatistics</div>;
+	const [data, setData] = useState([]);
+
+	const getExceptions = async () => {
+		const response = await fetch(baseUrl + '/admin/exception');
+		const exceptions = await response.json();
+		setData(exceptions);
+	};
+
+	useEffect(() => {
+		async function fetchData() {
+			await getExceptions();
+		}
+		fetchData();
+	}, []);
+
+	return <div>{JSON.stringify(data)}</div>;
 }

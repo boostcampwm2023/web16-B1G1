dedd4b92b7d771f839132b3920364e685870c836
qkrwogk <pjha999@naver.com>
2023-12-07 15:25:06 +0900

    ✨ POST /sentiment
    - 본문 글을 전달받아 감정 분석된 결과를 색상으로 변환하여 반환하는 API 구현
    - 네이버 감정분석 API 연동
    - fetch받은 결과를 에러처리 후 정상적으로 분석 결과가 나온 경우 색상 코드로 변환

--- packages/server/src/sentiment/sentiment.controller.ts
+++ packages/server/src/sentiment/sentiment.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SentimentService } from './sentiment.service';
+import { GetSentimentDto } from './dto/get-sentiment.dto';
 
 @Controller('sentiment')
 export class SentimentController {
-  constructor(private readonly sentimentService: SentimentService) {}
+	constructor(private readonly sentimentService: SentimentService) {}
+
+	@Post()
+	getSentiment(@Body() body: GetSentimentDto) {
+		return this.sentimentService.getSentiment(body);
+	}
 }

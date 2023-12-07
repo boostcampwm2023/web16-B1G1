51aa5a5156fdfdf9c65fb605fbfb4a2390a986f5
qkrwogk <pjha999@naver.com>
2023-12-08 01:34:37 +0900

    ✅ unit test on sentiment service
    - 감정분석 API의 모든 에러 처리를 커버하는 유닛 테스트 코드 작성

--- packages/server/test/sentiment/sentiment.service.spec.ts
+++ packages/server/test/sentiment/sentiment.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SentimentService } from '../../src/sentiment/sentiment.service';
+import {
+	BadRequestException,
+	InternalServerErrorException,
+} from '@nestjs/common';
 
 describe('SentimentService', () => {
 	let service: SentimentService;
@@ -10,9 +14,64 @@ describe('SentimentService', () => {
 		}).compile();
 
 		service = module.get<SentimentService>(SentimentService);
+
+		// jest.mock('fetch');
 	});
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
+	});
+
+	describe('getSentiment', () => {
+		it('should throw BadRequestException with empty content', async () => {
+			await expect(service.getSentiment({ content: '' })).rejects.toThrow(
+				'content is required',
+			);
+
+			await expect(service.getSentiment({ content: '' })).rejects.toThrow(
+				BadRequestException,
+			);
+		});
+
+		it('shold throw InternalServerErrorException with error', async () => {
+			jest.spyOn(global, 'fetch').mockResolvedValue({
+				json: async () => {
+					return { error: 'error' };
+				},
+			} as any);
+
+			await expect(service.getSentiment({ content: 'test' })).rejects.toThrow(
+				'sentiment api failed',
+			);
+
+			await expect(service.getSentiment({ content: 'test' })).rejects.toThrow(
+				InternalServerErrorException,
+			);
+		});
+
+		it('should throw InternalServerErrorException with no positive, negative, or neutral', async () => {
+			jest.spyOn(global, 'fetch').mockResolvedValue({
+				json: async () => {
+					return { document: { confidence: {} } };
+				},
+			} as any);
+
+			await expect(service.getSentiment({ content: 'test' })).rejects.toThrow(
+				'sentiment api failed',
+			);
+
+			await expect(service.getSentiment({ content: 'test' })).rejects.toThrow(
+				InternalServerErrorException,
+			);
+		});
+
+		it('should return sentiment', async () => {
+			const sentiment = await service.getSentiment({ content: 'test' });
+			expect(sentiment).toBeDefined();
+		});
+	});
+
+	afterEach(() => {
+		jest.restoreAllMocks();
 	});
 });

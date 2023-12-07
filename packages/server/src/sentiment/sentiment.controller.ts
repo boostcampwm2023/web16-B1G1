5efaf77c8bf21d4874688315f8385b5300b20b23
qkrwogk <pjha999@naver.com>
2023-12-07 15:38:50 +0900

    🐛 fix http code to 200
    - POST라서 201이 기본인데 200으로 성공 시 HTTP 코드 변경

--- packages/server/src/sentiment/sentiment.controller.ts
+++ packages/server/src/sentiment/sentiment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { SentimentService } from './sentiment.service';
 import { GetSentimentDto } from './dto/get-sentiment.dto';
 
@@ -7,6 +7,7 @@ export class SentimentController {
 	constructor(private readonly sentimentService: SentimentService) {}
 
 	@Post()
+	@HttpCode(200)
 	getSentiment(@Body() body: GetSentimentDto) {
 		return this.sentimentService.getSentiment(body);
 	}

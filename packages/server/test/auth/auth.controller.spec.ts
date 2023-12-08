aeba09a127c8ba4b043be0994cc23ac91172543f
송준섭 <wnstjq7979@gmail.com>
2023-12-08 20:35:19 +0900

    :white_check_mark: Set Auth Controller Test
    - Auth Controller 태스트를 위해 모듈 설정

--- packages/server/test/auth/auth.controller.spec.ts
+++ packages/server/test/auth/auth.controller.spec.ts
@@ -1,6 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../../src/auth/auth.controller';
 import { AuthService } from '../../src/auth/auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../src/auth/entities/user.entity';
+import { ShareLink } from '../../src/auth/entities/share-link.entity';
+import { JwtService } from '@nestjs/jwt';
+import { Galaxy } from '../../src/galaxy/schemas/galaxy.schema';
+import { Model } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { RedisRepository } from '../../src/auth/redis.repository';
 
 describe('AuthController', () => {
 	let controller: AuthController;
@@ -8,7 +17,27 @@ describe('AuthController', () => {
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [AuthController],
-			providers: [AuthService],
+			providers: [
+				AuthService,
+				{
+					provide: getRepositoryToken(User),
+					useClass: Repository,
+				},
+				{
+					provide: getRepositoryToken(ShareLink),
+					useClass: Repository,
+				},
+				RedisRepository,
+				JwtService,
+				{
+					provide: getModelToken(Galaxy.name),
+					useValue: Model,
+				},
+				{
+					provide: getModelToken('Exception'),
+					useValue: Model,
+				},
+			],
 		}).compile();
 
 		controller = module.get<AuthController>(AuthController);

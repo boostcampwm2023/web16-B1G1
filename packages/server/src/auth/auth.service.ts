8aae4d745e2bdf02fd2c40043058f9cd09c7c0ca
송준섭 <wnstjq7979@gmail.com>
2023-12-09 22:49:30 +0900

    :sparkles: Add checkNickname Api
    - Auth 모듈에 닉네임을 받아 해당 닉네임의 유저가 있는지 확인하는 checkNickname Api 구현

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -296,4 +296,18 @@ export class AuthService {
 
 		return linkUser.nickname;
 	}
+
+	checkNickname(nickname: string) {
+		if (!nickname) {
+			throw new BadRequestException('nickname is required');
+		}
+
+		const user = this.userRepository.findOneBy({ nickname });
+
+		if (!user) {
+			throw new NotFoundException('user not found');
+		}
+
+		return true;
+	}
 }

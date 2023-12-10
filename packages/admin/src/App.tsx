7428237631c93ed53bfa20aa71074959e65f5d7b
qkrwogk <pjha999@naver.com>
2023-12-10 16:54:00 +0900

    ✨ append login page to routes
    - 라우트와 메뉴바에 로그인 페이지 등록

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -5,6 +5,7 @@ import Board from './components/Board/Board.tsx';
 import Nav from './components/Nav.tsx';
 import SystemInfo from './components/SystemInfo/SystemInfo.tsx';
 import ExceptionStatistics from './components/ExceptionStatistics/ExceptionStatistics.tsx';
+import Login from './components/Login/Login.tsx';
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 				<Link to={'/admin/board'}>Board</Link>
 				<Link to={'/admin/system-info'}>System Info</Link>
 				<Link to={'/admin/exception-statistics'}>Exception Statistics</Link>
+				<Link to={'/admin/login'}>Login</Link>
 			</Nav>
 			<Routes>
 				<Route path="/admin" element={<div>Home</div>} />
@@ -23,6 +25,7 @@ function App() {
 					path="/admin/exception-statistics"
 					element={<ExceptionStatistics />}
 				/>
+				<Route path="/admin/login" element={<Login />} />
 			</Routes>
 		</>
 	);

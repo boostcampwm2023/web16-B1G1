294f6792d7bf6956ef814ecb0b8f1ee63275b2b2
bananaba <200tiger1@gmail.com>
2023-12-11 10:03:32 +0900

    ⚡️ Opimization
    - three.js 최적화를 진행했다

--- packages/client/src/widgets/landingScreen/LandingScreen.tsx
+++ packages/client/src/widgets/landingScreen/LandingScreen.tsx
@@ -3,23 +3,34 @@ import BackgroundStars from 'features/backgroundStars/BackgroundStars.tsx';
 import { Galaxy } from '../galaxy/index.ts';
 import { EffectComposer, Bloom } from '@react-three/postprocessing';
 import { CAMERA_POSITION, CAMERA_UP, CAMERA_FAR } from './lib/camera.ts';
+import { Group, Object3DEventMap } from 'three';
+import { useRef, useEffect } from 'react';
 
-interface PropsType {
-	mousePosition: number[];
-}
-
-export default function LandingScreen({
-	mousePosition: [mouseX, mouseY],
-}: PropsType) {
+export default function LandingScreen() {
+	const galaxyRef = useRef<Group<Object3DEventMap>>(null!);
 	const camera = {
 		position: CAMERA_POSITION,
 		up: CAMERA_UP,
 		far: CAMERA_FAR,
 	};
 
+	useEffect(() => {
+		const mouseHandler = (e: MouseEvent) => {
+			console.log(e);
+			const mouseX = e.clientX / window.innerWidth;
+			const mouseY = e.clientY / window.innerHeight;
+			galaxyRef.current.rotation.x = (mouseY - 0.5) / 5;
+			galaxyRef.current.rotation.y = (mouseX - 0.5) / 5;
+		};
+
+		window.addEventListener('mousemove', mouseHandler);
+
+		return () => window.removeEventListener('mousemove', mouseHandler);
+	}, []);
+
 	return (
 		<div style={{ height: '100vh', width: '100vw' }}>
-			<Canvas camera={camera} frameloop="demand">
+			<Canvas camera={camera}>
 				<EffectComposer>
 					<Bloom
 						intensity={0.4}
@@ -32,7 +43,7 @@ export default function LandingScreen({
 				<color attach="background" args={['#070614']} />
 				<ambientLight color="#fff" intensity={5} />
 				<BackgroundStars />
-				<group rotation={[(mouseY - 0.5) / 5, (mouseX - 0.5) / 5, 0]}>
+				<group ref={galaxyRef}>
 					<Galaxy />
 				</group>
 			</Canvas>
